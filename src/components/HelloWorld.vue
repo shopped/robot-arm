48449f9cc434e59c625f4e01b941b11624941505
Shopped <patrickgesarcarroll@gmail.com>
2020-05-18 19:49:32 -0700

    Rotation canvas simulation

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -43,20 +43,63 @@ export default {
 				4: "Wrist",
 				5: "Wrist Rotation",
 				6: "Clamp"
-			}
+			},
+			canvas: {},
+			cw: 0,
+			ch: 0,
 		}
 	},
 	mounted: function() {
 		const canvas1 = document.getElementById('rotation-canvas');
 		const canvas2 = document.getElementById('position-canvas');
-		const c1 = canvas1.getContext('2d');
-		const c2 = canvas2.getContext('2d');
-		c1.beginPath();
-		c1.moveTo(100, 100);
-		c1.lineTo(200, 200);
-		c1.lineWidth = 5;
-		c1.stroke();
-		console.log(c2);
+		const cr = canvas1.getContext('2d');
+		const cp = canvas2.getContext('2d');
+		this.canvas.rotation = cr;
+		this.canvas.position = cp;
+		cr.lineWidth = 5;
+		cp.lineWidth = 5;
+		this.drawRotationGuides(cr);
+		this.cw = canvas1.width;
+		this.ch = canvas2.height;
+	},
+	methods: {
+		drawRotationGuides: function(cr) {
+			cr.clearRect(0, 0, this.cw, this.ch);
+			cr.beginPath();
+			cr.strokeStyle = "#0000FF"
+			cr.moveTo(50, 50);
+			cr.lineTo(250, 50);
+			cr.stroke();
+			cr.beginPath();
+			cr.strokeStyle = "#FF0000"
+			cr.moveTo(250, 50);
+			cr.lineTo(450, 50);
+			cr.stroke();
+			cr.beginPath();
+			cr.strokeStyle = "#00FF00"
+			cr.moveTo(250, 50);
+			cr.lineTo(250, 250);
+			cr.stroke();
+		},
+		drawRotationInput: function(cr, deg) {
+			cr.beginPath();
+			cr.strokeStyle = "#000000";
+			cr.moveTo(250, 50);
+			const x = 250 - 200 * Math.cos(deg * Math.PI / 180);
+			const y = 50 + 200 * Math.sin(deg * Math.PI / 180);
+			cr.lineTo(x, y)
+			cr.stroke();
+		}
+	},
+	watch: {
+		position: {
+			//deep: true,
+			handler: function(val) {
+				const cr = this.canvas.rotation;
+				this.drawRotationGuides(cr);
+				this.drawRotationInput(cr, parseInt(val[0]));
+			}
+		}
 	}
 }
 </script>
